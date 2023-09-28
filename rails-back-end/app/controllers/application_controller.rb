b0b421c02a9c0237bcdfa26bbd61dcc2763edd7b
Daniel Rice <danielrice5627@gmail.com>
2023-09-27 19:33:34 -0700

    initial commit

---
+++ rails-back-end/app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+    include ActionController::Cookies
+    wrap_parameters format: []
+  
+    before_action :authorize
+  
+    def authorize
+      return render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+    end
+
+end
